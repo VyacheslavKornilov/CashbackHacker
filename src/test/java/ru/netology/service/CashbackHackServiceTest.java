8f21a574a46745e1f9fede0ef22d9b74ca0fa4da
VyacheslavKornilov <95097601+VyacheslavKornilov@users.noreply.github.com>
2022-03-17 17:34:51 +0300

    Add branch TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,31 +1,31 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.*;
+import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
     public void shouldBelowLimit() {
-        int amount = 900;
+        int amount = 800;
         int actual = service.remain(amount);
-        int expected = 100;
-        assertEquals(expected, actual);
+        int expected = 200;
+        assertEquals(actual, expected);
     }
     @Test
     public void shouldLimit() {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
     @Test
     public void shouldOverLimit () {
-        int amount = 1100;
+        int amount =1500;
         int actual = service.remain(amount);
-        int expected = 900;
-        assertEquals(expected, actual);
+        int expected = 500;
+        assertEquals(actual, expected);
     }
 }
